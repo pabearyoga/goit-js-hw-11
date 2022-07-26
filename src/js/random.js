05da405a523d7484628e02c4c86b796ee8381014
Pabearyoga <pabearyoga@gmail.com>
2022-07-26 15:54:50 +0300

    fix random.js

--- src/js/random.js
+++ src/js/random.js
@@ -1,17 +1,11 @@
 import randomWords from 'random-words';
 
-// if (document.querySelector('[name="searchQuery"').value === '') {
-//   document.querySelector('.search-btn').textContent = 'Random';
-// } else {
-//   document.querySelector('.search-btn').textContent = 'Search';
-// }
 export default class Random {
   constructor() {}
 
   randomSearch() {
     if (document.querySelector('.search-btn').textContent === 'Random') {
       document.querySelector('[name="searchQuery"').value = randomWords();
-      document.querySelector('.search-btn').click();
     }
   }
 
@@ -23,18 +17,3 @@ export default class Random {
     }
   }
 }
-
-// export default function randomSearch() {
-//   if (document.querySelector('[name="searchQuery"').value === '') {
-//     document.querySelector('[name="searchQuery"').value = randomWords();
-//     document.querySelector('.search-btn').click();
-//   }
-// }
-
-// export default function randomBtn() {
-//   if (document.querySelector('[name="searchQuery"').value === '') {
-//     document.querySelector('.search-btn').textContent = 'Random';
-//   } else {
-//     document.querySelector('.search-btn').textContent = 'Search';
-//   }
-// }
